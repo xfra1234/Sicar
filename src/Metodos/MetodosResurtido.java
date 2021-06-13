f414d9a51068f1ee70abc0ee0f31248add616f8c
usuario <usuario@LAPTOP-25ONKTML>
2021-06-13 14:20:31 -0500

    modificacion resurtido

--- src/Metodos/MetodosResurtido.java
+++ src/Metodos/MetodosResurtido.java
@@ -7,6 +7,7 @@ package Metodos;
 
 import java.awt.Desktop;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
@@ -21,7 +22,9 @@ import org.apache.poi.hssf.usermodel.HSSFRichTextString;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
 
 /**
  *
@@ -41,33 +44,214 @@ public class MetodosResurtido {
     conexion conectar = new conexion();
     String abrirarchivo = "", guardararchivo = "";
     int contador = 0, contador1 = 0;
-    float cantidad3ma = 0, cantidad3md = 0;
-    String descripcion = "";
+    float cantidad3ma = 0, cantidad3md = 0, existencia = 0;
+    String descripcion = "", descripcion2 = "", categoria = "", clave = "";
     int idpaquete;
     protected ArrayList<Integer> idNumeros = new ArrayList();
-    public void limpiarVariables(){
-        cantidad3ma=0;
-        cantidad3md=0;
-        contador=0;
-        contador1=0;
-        idpaquete=0;
+
+    public void limpiarVariables() {
+        cantidad3ma = 0;
+        cantidad3md = 0;
+        contador = 0;
+        contador1 = 0;
+        idpaquete = 0;
     }
 
     public void sucursales(String fecha1, String fecha2, String fechauno, String fechados, int sucursal, String mes) {
         switch (sucursal) {
             case 1:
+//                abrirarchivo = ("C:\\Users\\usuario\\Desktop\\Resurtido.xls");
+//                guardararchivo = ("C:\\Users\\usuario\\Desktop\\Resurtido de sucursal2.xls");
+                abrirarchivo = ("C:\\Users\\Cpu\\Documents\\Resurtido de sucursal.xls");
                 guardararchivo = ("C:\\Users\\Cpu\\Desktop\\Resurtido de sucursal_Magisterio de " + mes + ".xls");
-                resurtido(fecha1, fecha2, fechauno, fechados);
+                resurtidocopia(fecha1, fecha2, fechauno, fechados);
                 break;
             case 2:
+
+                abrirarchivo = ("C:\\Users\\GHIA\\Documents\\Resurtido de sucursal.xls");
                 guardararchivo = ("C:\\Users\\GHIA\\Desktop\\Resurtido de sucursal_Coapinole de" + mes + ".xls");
-                resurtido(fecha1, fecha2, fechauno, fechados);
+                resurtidocopia(fecha1, fecha2, fechauno, fechados);
                 break;
             case 3:
+                abrirarchivo = ("C:\\Users\\GHIA\\Documents\\Resurtido de sucursal.xls");
                 guardararchivo = ("C:\\Users\\GHIA\\Desktop\\Resurtido de sucursal_ Bodega de" + mes + ".xls");
-                resurtido(fecha1, fecha2, fechauno, fechados);
+                resurtidocopia(fecha1, fecha2, fechauno, fechados);
                 break;
         }
+    }
+
+    public void resurtidocopia(String fecha1ma, String fecha3ma, String fecha1md, String fecha3md) {
+
+        try ( FileInputStream file = new FileInputStream(new File(abrirarchivo))) {
+            POIFSFileSystem fs = new POIFSFileSystem(file);
+            HSSFWorkbook libro = new HSSFWorkbook(fs);
+            //obtener la hoja que se va leer
+            HSSFSheet hoja = libro.getSheetAt(0);
+            //obtener todas las filas de la hoja excel
+            HSSFCell celda;
+            HSSFRow fila;
+            Row row;
+            /// creacion de estilos de letra 
+            //formato para el nombre de las columnas
+            CellStyle headerStyle = libro.createCellStyle();
+            HSSFFont font = libro.createFont();
+            font.setBold(true);
+            font.setFontName("Arial");
+            font.setFontHeight((short) (14 * 20));
+            headerStyle.setFont(font);
+
+            //formato para encabezados
+            CellStyle encabezados = libro.createCellStyle();
+            HSSFFont font2 = libro.createFont();
+            font2.setBold(true);
+            font2.setFontName("Arial");
+            font2.setFontHeight((short) (10 * 20));
+            encabezados.setFont(font2);
+
+            CellStyle letraprincipal = libro.createCellStyle();
+            HSSFFont font3 = libro.createFont();
+            font3.setBold(true);
+            font3.setFontName("Arial");
+            font3.setFontHeight((short) (15 * 20));
+            letraprincipal.setFont(font3);
+
+            fila = hoja.createRow(9);
+            celda = fila.createCell(1);
+            celda.setCellValue("olo");
+            celda.setCellStyle(encabezados);
+
+            int filaa = 9;
+            try {
+
+                con = conectar.conectarMySQL();
+                stmt = con.createStatement();
+                rs = stmt.executeQuery("select articulo.art_id,articulo.clave,articulo.existencia"
+                        + "articulo.descripcion,categoria.nombre"
+                        + " from articulo inner join categoria on categoria.cat_id = articulo.cat_id "
+                        + " where articulo.status !=-1  order by categoria.nombre,articulo.descripcion");
+                while (rs.next()) {
+                    int idart = rs.getInt(1);
+                    clave = rs.getString(2);
+                    existencia = rs.getFloat(3);
+                    descripcion = rs.getString(4);
+                    categoria = rs.getString(5);
+
+                    con2 = conectar.conectarMySQL();
+                    stmt2 = con2.createStatement();
+                    rs2 = stmt2.executeQuery("select sum(cantidad) from detallev "
+                            + "inner join venta on venta.ven_id = detallev.ven_id "
+                            + " where detallev.art_id=" + idart + " and "
+                            + " venta.fecha between '" + fecha1ma + "' and '" + fecha3ma + "'"
+                            + " and venta.status!=-1");
+                    if (rs2.next()) {
+                        cantidad3ma = cantidad3ma + rs2.getFloat(1);
+                    }
+                    con2.close();
+
+                    con2 = conectar.conectarMySQL();
+                    stmt2 = con2.createStatement();
+                    rs2 = stmt2.executeQuery("select sum(cantidad) from detallev "
+                            + "inner join venta on venta.ven_id = detallev.ven_id "
+                            + " where detallev.art_id=" + idart + " and "
+                            + " venta.fecha between '" + fecha1md + "' and '" + fecha3md + "'"
+                            + " and venta.status!=-1");
+                    if (rs2.next()) {
+                        cantidad3md = cantidad3md + rs2.getFloat(1);
+                    }
+                    con2.close();
+
+                    if (categoria.equals(descripcion2)) {
+                        fila = hoja.createRow(filaa);
+//                    
+                        celda = fila.createCell(0);
+                        celda.setCellValue(clave);
+                        celda.setCellStyle(encabezados);
+
+                        celda = fila.createCell(1);
+                        celda.setCellValue(new HSSFRichTextString(descripcion));
+                        celda.setCellStyle(encabezados);
+
+                        celda = fila.createCell(2);
+                        celda.setCellValue(existencia);
+                        celda.setCellStyle(encabezados);
+
+                        celda = fila.createCell(4);
+                        celda.setCellValue(existencia);
+                        celda.setCellStyle(encabezados);
+
+                        celda = fila.createCell(6);
+                        celda.setCellValue(cantidad3ma);
+                        celda.setCellStyle(encabezados);
+
+                        celda = fila.createCell(7);
+                        celda.setCellValue(cantidad3md);
+                        celda.setCellStyle(encabezados);
+//                    
+                        filaa = filaa + 1;
+                    } else {
+                        filaa = filaa + 1;
+                        fila = hoja.createRow(filaa);
+                        celda = fila.createCell(0);
+                        celda.setCellValue(categoria);
+                        celda.setCellStyle(letraprincipal);
+
+                        filaa = filaa + 1;
+                        fila = hoja.createRow(filaa);
+                        celda = fila.createCell(0);
+                        celda.setCellValue(clave);
+                        celda.setCellStyle(encabezados);
+
+                        celda = fila.createCell(1);
+                        celda.setCellValue(new HSSFRichTextString(descripcion));
+                        celda.setCellStyle(encabezados);
+
+                        celda = fila.createCell(2);
+                        celda.setCellValue(existencia);
+                        celda.setCellStyle(encabezados);
+
+                        celda = fila.createCell(4);
+                        celda.setCellValue(existencia);
+                        celda.setCellStyle(encabezados);
+
+                        celda = fila.createCell(6);
+                        celda.setCellValue(cantidad3ma);
+                        celda.setCellStyle(encabezados);
+
+                        celda = fila.createCell(7);
+                        celda.setCellValue(cantidad3md);
+                        celda.setCellStyle(encabezados);
+//                    
+//                    
+                        filaa = filaa + 1;
+                        descripcion2 = categoria;
+                    }
+
+                    existencia = 0;
+                    cantidad3ma = 0;
+                    cantidad3md = 0;
+                }
+
+                //////////////////////////////////////////////////////////////////
+              
+                    existencia = 0;
+                    cantidad3ma = 0;
+                    cantidad3md = 0;
+                
+        
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e);
+                e.printStackTrace();
+            }
+
+            FileOutputStream elFichero = new FileOutputStream(guardararchivo);
+            libro.write(elFichero);
+            elFichero.close();
+            File archivo = new File(guardararchivo);
+            Desktop.getDesktop().open(archivo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void resurtido(String fecha1ma, String fecha3ma, String fecha1md, String fecha3md) {
@@ -353,7 +537,7 @@ public class MetodosResurtido {
                 if (rs2.next()) {
 
                 } else {
-                   
+
                     idNumeros.add(id);
                     contador = contador + 1;
                 }
@@ -370,26 +554,23 @@ public class MetodosResurtido {
                 rs = stmt.executeQuery("select paquete.paquete from paquete where paquete.articulo=" + valor + "");
                 if (rs.next()) {
 
-                  
                     con2 = conectar.conectarMySQL();
                     stmt2 = con2.createStatement();
                     rs2 = stmt2.executeQuery("select paquete.paquete,paquete.cantidad from paquete where paquete.articulo=" + valor + "");
                     while (rs2.next()) {
-                             id= rs2.getInt(1);
+                        id = rs2.getInt(1);
                         con3 = conectar.conectarMySQL();
                         stmt3 = con3.createStatement();
-                        rs3 = stmt3.executeQuery("select existencia,status from articulo where art_id='"+id+"'"
+                        rs3 = stmt3.executeQuery("select existencia,status from articulo where art_id='" + id + "'"
                                 + " and status !=-1");
-                       
-                            while (rs3.next()) {
-                            existencia = existencia +( rs3.getFloat(1)*rs2.getFloat(2));
+
+                        while (rs3.next()) {
+                            existencia = existencia + (rs3.getFloat(1) * rs2.getFloat(2));
                         }
-                        
-                        
+
                         con3.close();
                     }
                     con2.close();
-                    
 
                     con2 = conectar.conectarMySQL();
                     stmt2 = con2.createStatement();
@@ -443,7 +624,6 @@ public class MetodosResurtido {
                     }
                     con2.close();
 
-                  
                     con2 = conectar.conectarMySQL();
                     stmt2 = con2.createStatement();
                     rs2 = stmt2.executeQuery("select articulo.clave,articulo.existencia,articulo.descripcion,categoria.nombre"
