4b0fdb5de191546b3c6abd56b166ab00a0241242
usuario <usuario@LAPTOP-25ONKTML>
2021-11-07 13:22:21 -0600

    acomodo de las notas de crédito

--- src/Metodos/MetodosReporteDepartamento.java
+++ src/Metodos/MetodosReporteDepartamento.java
@@ -219,7 +219,7 @@ public class MetodosReporteDepartamento {
                 while (rs.next()) {
                     mes = rs.getString(1);
                     anio = rs.getString(3);
-                    cantidad= rs.getFloat(2);
+                    cantidad= rs.getFloat(2)-(ventas[totalmeses]*media);
                     JOptionPane.showMessageDialog(null, ventas[totalmeses]);
                     totalmeses = totalmeses + 1;
 //                    mes = mes.toUpperCase().charAt(0) + mes.substring(1, mes.length());
@@ -271,7 +271,7 @@ public class MetodosReporteDepartamento {
                 while (rs.next()) {
                     mes = rs.getString(1);
                     anio = rs.getString(3);
-                    cantidad= rs.getFloat(2);
+                    cantidad= rs.getFloat(2)-(ventas[totalmeses]*baja);
                     totalmeses = totalmeses + 1;
 //                    mes = mes.toUpperCase().charAt(0) + mes.substring(1, mes.length());
 //
