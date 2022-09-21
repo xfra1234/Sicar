60f679f2a4fe261ba852bb178caf13aa9a758cd6
usuario <usuario@LAPTOP-25ONKTML>
2022-09-20 21:22:18 -0500

    configuracion reporte metodos resurtido solo productos este año

--- src/Metodos/MetodosResurtidosoloproductos_esteaño.java
+++ src/Metodos/MetodosResurtidosoloproductos_esteaño.java
@@ -534,7 +534,7 @@ public class MetodosResurtidosoloproductos_esteaño {
                             preciocompra = (float) (rs2.getFloat(5) *iva);
                         }
                         
-                         if (valor ==1363){
+                         if (contador ==1363){
                             System.out.println(valor);
                         }
                         
