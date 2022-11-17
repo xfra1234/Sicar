faa0e6beeab5214d08811e962ebf097ba4714d6a
usuario <usuario@LAPTOP-25ONKTML>
2022-11-16 20:10:12 -0600

    modificacion resurtidos solo productos

--- src/Metodos/MetodosResurtidosoloproductos_esteaño.java
+++ src/Metodos/MetodosResurtidosoloproductos_esteaño.java
@@ -549,7 +549,7 @@ public class MetodosResurtidosoloproductos_esteaño {
 //                            celda.setCellStyle(encabezados);
 
                             celda = fila.createCell(0);
-                            celda.setCellValue(new HSSFRichTextString(nombrearticulo+" ");
+                            celda.setCellValue(new HSSFRichTextString(nombrearticulo+" "));
                             celda.setCellStyle(encabezados);
 
                             celda = fila.createCell(1);
