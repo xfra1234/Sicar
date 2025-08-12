2da33d92a8565aa36f519bd5eb8cda1d1f14fb5d
coron <coron@Francisco>
2025-08-12 16:49:31 -0600

    regresar a como estaban los reportes

--- src/reportes/frmReporteResurtido.java
+++ src/reportes/frmReporteResurtido.java
@@ -36,9 +36,7 @@ public class frmReporteResurtido extends javax.swing.JFrame {
         initComponents();
         cmbmes.setDate(new Date());
         rbtnbodega.setSelected(true);
-        rdbanterio_nuevo.add(rbtnanterior);
-        rdbanterio_nuevo.add(rbtnnuevo);
-        rbtnanterior.isSelected();
+        
 
     }
 
@@ -59,10 +57,7 @@ public class frmReporteResurtido extends javax.swing.JFrame {
         } else {
             sucursal = 0;
         }
-        if (rbtnnuevo.isSelected()) {
-              metfecha.generarfecha1mes(cmbmes.getDate(), sucursal);
-        }
-      
+        metfecha.generarfecha1mes(cmbmes.getDate(), sucursal);
         return sucursal;
     }
 
@@ -152,8 +147,6 @@ public class frmReporteResurtido extends javax.swing.JFrame {
         rbtnbodegapdv = new javax.swing.JRadioButton();
         rbtnmojoneras = new javax.swing.JRadioButton();
         rbtnpitillal = new javax.swing.JRadioButton();
-        rbtnnuevo = new javax.swing.JRadioButton();
-        rbtnanterior = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Reporte Resurtido de Sucursal");
@@ -212,32 +205,19 @@ public class frmReporteResurtido extends javax.swing.JFrame {
         rbtnpitillal.setText("Pitillal");
         rbtnpitillal.setOpaque(false);
 
-        rbtnnuevo.setText("Nuevo");
-        rbtnnuevo.setOpaque(false);
-
-        rbtnanterior.setText("Anterior");
-        rbtnanterior.setOpaque(false);
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(btnRegresar, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(btnRegresar, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(jLabel1))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(103, 103, 103)
-                        .addComponent(cmbmes, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(131, 131, 131)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel2)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(20, 20, 20)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(rbtnbodega)
                             .addGroup(jPanel1Layout.createSequentialGroup()
@@ -245,17 +225,20 @@ public class frmReporteResurtido extends javax.swing.JFrame {
                                 .addGap(26, 26, 26)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(rbtnmagisterio)
-                                    .addComponent(rbtnbodegapdv))
+                                    .addComponent(rbtnbodegapdv)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGap(21, 21, 21)
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(jLabel2)
+                                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE))))
                                 .addGap(26, 26, 26)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(rbtncoapinole)
-                                    .addComponent(rbtnpitillal)))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(60, 60, 60)
-                                .addComponent(rbtnnuevo)
-                                .addGap(30, 30, 30)
-                                .addComponent(rbtnanterior)))))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                    .addComponent(rbtnpitillal))))
+                        .addGap(23, 23, 23))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(cmbmes, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(70, 70, 70))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -265,9 +248,9 @@ public class frmReporteResurtido extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(18, 18, 18)
                         .addComponent(jLabel1)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(cmbmes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(rbtnbodega)
                     .addComponent(rbtnmagisterio)
@@ -277,14 +260,11 @@ public class frmReporteResurtido extends javax.swing.JFrame {
                     .addComponent(rbtnbodegapdv)
                     .addComponent(rbtnmojoneras)
                     .addComponent(rbtnpitillal))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 10, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(rbtnanterior)
-                    .addComponent(rbtnnuevo))
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel2)
+                .addGap(20, 20, 20))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -313,13 +293,9 @@ public class frmReporteResurtido extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         //  Mesesañoanterior();
-        if (rbtnanterior.isSelected()) {
-            Mesesañoanterior();
-            System.out.println("holo");
-        } else {
+      
             valorsucursal();
-            System.out.println("Ño");
-        }
+        
 
 
     }//GEN-LAST:event_jButton1ActionPerformed
@@ -369,13 +345,11 @@ public class frmReporteResurtido extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JRadioButton rbtnanterior;
     private javax.swing.JRadioButton rbtnbodega;
     private javax.swing.JRadioButton rbtnbodegapdv;
     private javax.swing.JRadioButton rbtncoapinole;
     private javax.swing.JRadioButton rbtnmagisterio;
     private javax.swing.JRadioButton rbtnmojoneras;
-    private javax.swing.JRadioButton rbtnnuevo;
     private javax.swing.JRadioButton rbtnpitillal;
     private javax.swing.ButtonGroup rdbComercios;
     private javax.swing.ButtonGroup rdbanterio_nuevo;
